6f419f63b489379eb2cbe6cf46230404fb6b2265
paulo <paulo.araujo@splab.ufcg.edu.br>
2017-12-14 10:28:30 -0300

    Initial commit

---
+++ code/simulador/src/simulador/Simulador.java
@@ -0,0 +1,11 @@
+package simulador;
+
+public class Simulador {
+	
+	// Considere o trecho de código abaixo independente da classe acima.
+	
+	public static void main(String[] args) {
+		// TODO recebe arumentos para rodar a simulação
+		// 
+	}
+}

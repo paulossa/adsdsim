c90a789450af027dbd9a6813fad10ae2b85248c0
paulo <paulo.araujo@splab.ufcg.edu.br>
2017-12-20 08:53:52 -0300

    Adding duration increasing logic on server. neeeds to generate correct output and add other distributions

--- code/simulador/src/simulador/Server.java
+++ code/simulador/src/simulador/Server.java
@@ -1,19 +1,44 @@
 package simulador;
 
 import java.util.LinkedList;
+import java.util.function.Predicate;
 
 import org.apache.commons.math3.distribution.ExponentialDistribution;
 
 public class Server {
 	
-	private LinkedList queue;
+	private LinkedList<Double> queue;
 	private double avgServiceTime;
 	private ExponentialDistribution expDist;
+	private boolean busy = false;
+	private int requestsAttended;
 
 	public Server(double avgServiceTime) {
-		this.queue = new LinkedList<>();
+		this.queue = new LinkedList<Double>();
 		this.avgServiceTime = avgServiceTime;
 		this.expDist = new ExponentialDistribution(avgServiceTime);
+	}
+
+	public void receivesRequest(double presentTime) {
+		this.queue.add(presentTime + Math.floor(expDist.sample()));
+	}
+
+	private boolean isBusy() { return busy;	}
+
+	/**
+	 * Ao ser chamado esse método irá verificar se coisas precisam ser processadas 
+	 * ou finalizaram de ser processadas e atualizar o servidor de acordo. 
+	 * @param presentTime
+	 */
+	public void clock(int presentTime) {
+		busy = (queue.size() > 0);
+		
+		final double paramTime = presentTime;
+		Predicate<Double> isCurrentTime = t -> t == paramTime;
+		if (queue.removeIf(isCurrentTime)) {
+			requestsAttended += 1;
+		}
+		
 	}
 	
 	
--- code/simulador/src/simulador/Simulador.java
+++ code/simulador/src/simulador/Simulador.java
@@ -2,6 +2,7 @@ package simulador;
 
 import org.apache.commons.math3.distribution.*;
 import java.util.Date;
+import java.util.LinkedList;import java.util.function.Predicate;
 
 public class Simulador {
 	
@@ -13,8 +14,9 @@ public class Simulador {
 	private int repetitions, duration;
 	private double mean;
 	private AbstractRealDistribution realDist;
-	private long startTime;
 	private Server server;
+	private LinkedList<Double> requests;
+	private int requestsSent = 0;
 	
 	
 	public Simulador(int distribution, double mean, int duration, int repetitions) {
@@ -23,16 +25,8 @@ public class Simulador {
 		this.mean = mean;
 		setDist(distribution);
 		this.duration = duration;		
-		this.startTime = new Date().getTime();
 		this.server = new Server(mean);
-	}
-	
-	private long getCurrentTime() {
-		return new Date().getTime();
-	}
-	
-	private void setStartTime() {
-		this.startTime = new Date().getTime();
+		this.requests = new LinkedList<>();
 	}
 	
 	public long totalTime(long timeInit, long timeFinish) {
@@ -40,57 +34,67 @@ public class Simulador {
 	}
 	
 	public void simulate() throws InterruptedException {
-		setStartTime(); //tempo inicial da execução do programa
 		int total = 0;	
 		for (int i = 0; i < repetitions; i++) {
-			long startTimeRepetiton = new Date().getTime(); // tempo inicial de cada repetição
-			int count = 0;
-			while ((getCurrentTime() - startTimeRepetiton <= this.duration)) {
-//				sendConsumer();
-				System.out.println((getGenerator().sample() * 1000));
-//				Thread.sleep( (long) (getGenerator().sample() * 1000));
-				count++;	
+			server = new Server(mean);
+			requests = new LinkedList();
+			
+			for (int presentTime = 0; presentTime < duration; presentTime++) {
+				double requestTime = presentTime + Math.floor(getGenerator().sample());
+				if (requests.size() < 2) {
+					requests.add(requestTime);
+					System.out.println(String.format("%d %s", presentTime, requests.toString()));
+				}
+				
+				
+				final int paramTime = presentTime;
+				Predicate<Double> isCurrentTime = t -> t == paramTime;
+				boolean elementsRemoved = requests.removeIf(isCurrentTime);
+				if (elementsRemoved) {
+					server.receivesRequest(presentTime);
+					requestsSent++;
+				}
+				server.clock(presentTime);
+//				count++;	
 			}
-			total += count;
-			System.out.println("---------------------- " + "Tipo da distribuição: " + nameDist(distribution));
-			System.out.println("---------------------- " + "Parâmentro da distribuição: " + mean);
-			System.out.println("---------------------- " + "Repetição: " + i); // qual repetição está
-			System.out.println("---------------------- " + "Tempo da repetição: " + (getCurrentTime() - startTimeRepetiton));
-			System.out.println("---------------------- " + "Tempo médio de atendimento - repetição: " + ((float)(getCurrentTime() - startTimeRepetiton)/count)); //tentar usando bigdecimal
-			System.out.println("---------------------- " + "Quantidade de elementos por repetição: " + count); // imprime quantidade por repetição
+			
+			
+			
+//			total += count;
+//			System.out.println("---------------------- " + "Tipo da distribuição: " + nameDist(distribution));
+//			System.out.println("---------------------- " + "Parâmentro da distribuição: " + mean);
+//			System.out.println("---------------------- " + "Repetição: " + i); // qual repetição está
+//			System.out.println("---------------------- " + "Tempo da repetição: " + (getCurrentTime() - startTimeRepetiton));
+//			System.out.println("---------------------- " + "Tempo médio de atendimento - repetição: " + ((float)(getCurrentTime() - startTimeRepetiton)/count)); //tentar usando bigdecimal
+//			System.out.println("---------------------- " + "Quantidade de elementos por repetição: " + count); // imprime quantidade por repetição
 			
 		}
-		System.out.println("\nTotal de elementos: " + total); // imprime o total de elementos das x repetições
-		System.out.println("Tempo total: " + totalTime(this.startTime, getCurrentTime())); //imprime o tempo total somado as x repetições
-		System.out.println("Tempo médio de atendimento - total: " + (float)(getCurrentTime() - this.startTime)/total);
+//		System.out.println("\nTotal de elementos: " + total); // imprime o total de elementos das x repetições
+//		System.out.println("Tempo total: " + totalTime(this.startTime, getCurrentTime())); //imprime o tempo total somado as x repetições
+//		System.out.println("Tempo médio de atendimento - total: " + (float)(getCurrentTime() - this.startTime)/server.something()?total);
 	}
 	
 
 	private void setDist(int distribution) {
 		switch (distribution) {
-		case NORMAL_DIST: 	realDist = new NormalDistribution();
-			break;
-		case EXP_DIST:
-			System.out.println("mean = " + mean);
-			realDist = new ExponentialDistribution(mean);
-			break;
-		case UNIFORM_DIST: 	realDist = new UniformRealDistribution();
-			break;
+			case NORMAL_DIST: 	realDist = new NormalDistribution(); 			break;
+			case EXP_DIST: 		realDist = new ExponentialDistribution(mean); 	break;
+			case UNIFORM_DIST: 	realDist = new UniformRealDistribution();    	break;
 		}
 	}
 	
 	private String nameDist(int distribution) {
 		switch(distribution) {
-		case 1: return "Normal";
-		case 2: return "Exponencial";
-		case 3: return "Uniforme";
+			case 1: return "Normal";
+			case 2: return "Exponencial";
+			case 3: return "Uniforme";
 		}
 		return "";
 	}
 	
 	private AbstractRealDistribution getGenerator() { return realDist; }
 
-	private void sendConsumer() {
+	private void sendRequestToServer() {
 		
 	}
 
@@ -98,19 +102,11 @@ public class Simulador {
 	
 	public static void main(String[] args) throws InterruptedException {
 
-//		UniformIntegerDistribution	// FIX constructor
-//		UniformRealDistribution	// FIX constructor
-
-
+		int repetitions = 20;
+		int duration = 5000;
 		
-		
-		Simulador simulador = new Simulador(Simulador.EXP_DIST, 0.5, 3, 3);
+		Simulador simulador = new Simulador(Simulador.EXP_DIST, 25, duration, repetitions);
 		simulador.simulate();
-		
-		
-		System.out.println("Hello bitch");
-		
-		
-
+		System.out.println("END");
 	}
 }
